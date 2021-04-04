e0f95c35ae4a8994ba5b10d44c80e8e975ba637f
Дрожкин Александр Андреевич <Santay2009@yandex.ru>
2021-04-04 15:11:16 +0300

    Добавьте файлы проекта.

---
+++ Frontend/WpfApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+
+using GrpcProto.Service;
+
+using System.Windows;
+
+namespace WpfApp
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+        internal HelloService Client { get; private set; }
+
+#region Overrides of Application
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			using GrpcChannel channel = GrpcChannel.ForAddress(@"https://localhost:5001");
+			Client = new HelloService();
+		}
+
+		#endregion
+	}
+}
---
+++ Frontend/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using GrpcService;
+
+using System.Windows;
+
+namespace WpfApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow() => InitializeComponent();
+
+		private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			HelloReply response = await (Application.Current as App).Client.SayHelloAsync(new HelloRequest() { Name = TextBoxName.Text });
+			TextBlockResponse.Text = response.Message;
+		}
+	}
+}
