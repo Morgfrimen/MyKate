3b4cb3bbd1c0e50b1a5955930d6dccddf5405a92
Дрожкин Александр Андреевич <Santay2009@yandex.ru>
2021-04-04 23:12:02 +0300

    Add TableValueServices

---
+++ GrpcService/Service/TableValueService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+using Grpc.Core;
+
+using Table;
+
+namespace GrpcProtoClient.Service
+{
+    public sealed class TableValueService : Table.TableService.TableServiceBase
+    {
+#region Overrides of TableServiceBase
+
+        public override Task<TableResponse> ChangeTableValue(TableRequest request, ServerCallContext context)
+        {
+            //TODO: model
+            return base.ChangeTableValue(request, context);
+        }
+
+#endregion
+    }
+}
