7fc5f819fe36c21ccad5170077ff1dfd23e0a493
Александр Дрожкин <Santay2009@yandex.ru>
2021-05-01 20:40:41 +0300

    Change namespace

---
+++ Frontend/WpfApp/ViewModels/Page/StartPageViewModels.cs
@@ -0,0 +1,27 @@
+﻿namespace WpfApp.ViewModels.Page
+{
+    public sealed class StartPageViewModels : ViewModelsBase
+    {
+        public StartPageViewModels()
+        {
+            GetMuvo();
+        }
+
+        private void GetMuvo()
+        {
+            MuvoList = (App.Current as App).ListMuvo;
+        }
+
+        private string[] _muvoList;
+
+        public string[] MuvoList
+        {
+            get => _muvoList;
+            private set
+            {
+                _muvoList = value;
+                OnPropertyChanged(nameof(MuvoList));
+            }
+        }
+    }
+}
