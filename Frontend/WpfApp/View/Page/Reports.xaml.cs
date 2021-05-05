5e9e469181e38a9fcf051b28a8edaa7501a41cc2
Александр Дрожкин <Santay2009@yandex.ru>
2021-05-05 14:12:49 +0300

    Crearmup code

--- Frontend/WpfApp/View/Page/Reports.xaml.cs
+++ Frontend/WpfApp/View/Page/Reports.xaml.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Input;
 
 namespace WpfApp.View.Page
 {
     /// <summary>
-    /// Логика взаимодействия для Reports.xaml
+    ///     Логика взаимодействия для Reports.xaml
     /// </summary>
     public partial class Reports : System.Windows.Controls.Page
     {
-        public static RoutedCommand NavigateReport31 = new ();
-        
+        public static RoutedCommand NavigateReport31 = new();
+
         public Reports()
         {
             InitializeComponent();
