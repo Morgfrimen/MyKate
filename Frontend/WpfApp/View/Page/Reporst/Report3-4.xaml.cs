2265de373068a6789bfe0a6956a3684277c00332
Александр Дрожкин <Santay2009@yandex.ru>
2021-06-15 00:24:16 +0300

    Add report 3-4

---
+++ Frontend/WpfApp/View/Page/Reporst/Report3-4.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp.View.Page.Reporst
+{
+	/// <summary>
+	/// Логика взаимодействия для Report3_4.xaml
+	/// </summary>
+	public partial class Report3_4 : ReportBaseView
+	{
+		public Report3_4()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- Frontend/WpfApp/View/Page/SelectReport.xaml.cs
+++ Frontend/WpfApp/View/Page/SelectReport.xaml.cs
@@ -11,20 +11,26 @@ namespace WpfApp.View.Page
     {
         public static RoutedCommand NavigateReport3_1 = new();
         public static RoutedCommand NavigateReport3_3 = new();
+        public static RoutedCommand NavigateReport3_4 = new();
 
         private void CommandBinding_OnExecuted_Report3_1(object sender, ExecutedRoutedEventArgs e)
         {
             MainWindow.FrameMainWindow.Navigate(new Report3_1());
         }
 
-        public SelectReport()
-        {
-            InitializeComponent();
-        }
-
         private void CommandBinding_OnExecuted_Report3_3(object sender, ExecutedRoutedEventArgs e)
         {
             MainWindow.FrameMainWindow.Navigate(new Report3_3());
+        }
+
+        private void CommandBinding_OnExecuted_Report3_4(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainWindow.FrameMainWindow.Navigate(new Report3_4());
+        }
+
+        public SelectReport()
+        {
+            InitializeComponent();
         }
     }
 }
