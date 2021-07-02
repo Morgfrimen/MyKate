798012afdc9f84a9a8b758dfcbe5f2c598b2d7ba
Александр Дрожкин <Santay2009@yandex.ru>
2021-07-02 12:16:09 +0300

    Add report 3_4

---
+++ Frontend/WpfApp/Models/Reports/Report3_4Models.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfApp.Models.Reports
+{
+    public sealed class Report3_4Models
+    {
+        public uint Number { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public decimal Sum { get;set; }
+        public string Remark { get; set; }
+    }
+}
