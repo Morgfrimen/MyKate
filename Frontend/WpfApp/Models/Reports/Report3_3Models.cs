0a7c4d0609e14ece4d799df9a9fbcc5f14502957
Александр Дрожкин <Santay2009@yandex.ru>
2021-07-02 11:52:42 +0300

    Add Models Report3_3

---
+++ Frontend/WpfApp/Models/Reports/Report3_3Models.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WpfApp.Models.Reports
+{
+    public sealed class Report3_3Models
+    {
+        public uint NumberPosition { get; set; }
+        public string NumberPurchase { get; set; }
+        public string PurchaseSubject { get; set; }
+        public string NameCounterParty { get; set; }
+        public string ContractDetails { get; set; }
+        public decimal ContractPrice { get; set; }
+        public decimal ActualPayment { get; set; }
+    }
+}
