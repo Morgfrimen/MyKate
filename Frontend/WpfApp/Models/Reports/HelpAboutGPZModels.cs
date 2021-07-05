f5e66619c2f0378377306186c73f1ceca4b98e6b
Александр Дрожкин <Santay2009@yandex.ru>
2021-07-06 02:15:49 +0300

    Common report completed

---
+++ Frontend/WpfApp/Models/Reports/HelpAboutGPZModels.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfApp.Models.Reports
+{
+    public sealed class HelpAboutGPZModels
+    {
+        public string Division { get; set; }
+        public ParaValue Plan { get; set; }
+        public ParaValue Zakl { get; set; }
+        public ParaValue Provedeno { get; set; }
+        public ParaValue Result { get; set; }
+        public string ProcGPZ { get; set; }
+    }
+
+    public sealed class ParaValue
+    {
+        public int Count { get; set; }
+        public string Sum { get; set; }
+    }
+}
