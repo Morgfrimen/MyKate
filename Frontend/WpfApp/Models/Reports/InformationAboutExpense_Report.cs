12cd7cc2bc4e054137531fec2f1d68e817a1269b
Александр Дрожкин <Santay2009@yandex.ru>
2021-07-06 00:00:11 +0300

    Create table informationAbout

---
+++ Frontend/WpfApp/Models/Reports/InformationAboutExpense_Report.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp.Models.Reports
+{
+    public sealed class InformationAboutExpense_Report
+    {
+        public string Division { get; set; }
+        public string FactZak { get; set; }
+        public string RachetValue { get; set; }
+        public string Dogovor { get; set; }
+        public string PriseChet { get; set; }
+        public string All { get; set; }
+        public string Result { get; set; }
+    }
+}
